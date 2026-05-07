ad24bece35044a377993d18ddf9347105ec19db3
suy77 <suellenquintela@gmail.com>
2026-05-07 13:28:28 -0300

    Update pedra_papel_tesoura.py

--- suy(ativ do mario)/atividadeeeee/pedra_papel_tesoura.py
+++ suy(ativ do mario)/atividadeeeee/pedra_papel_tesoura.py
@@ -1,9 +1,24 @@
-vc = input("escolha entre pedra, papel e tesoura: ")
 import random
-lista = ["pedra", "papel", "tesoura"]
-escolha = random.choice(lista)
-print(escolha)
-while true:
-    if vc == escolha:
-        print("voce perdeu")
-# eu vou surtarrrrrr
+
+lista = ['papel', 'pedra', 'tesoura']
+while True:
+    jogador = (0)
+    maquina = (0)
+      jogador < 3 and maquina < 3:
+
+     jogador = input("Escolha entre pedra, papel e tesoura: ").lower()
+     maquina = random.choice(lista)
+     print("a maquina jogou: ",(maquina))
+
+    if jogador not in lista:
+        print("Essa opção não existe, amigo.")
+
+    elif jogador == maquina:
+        print("deu empate :P")
+
+    if (jogador == 'pedra' and maquina == 'pappel') or \
+    
+
+    #else:
+     #   print("ponto pra maquina")
+      #  maquina = maquina+1
